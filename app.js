70494b5fe5305b6f8c5501d18e5476d199a647c5
romainDM <34089511+romainDM@users.noreply.github.com>
2018-03-06 15:20:53 +0100

    Grosse mise à jour
    Ajoute d'une map, du menu, ajout de ngrok,  d'une div pour cacher le menu

--- app.js
+++ app.js
@@ -10,3 +10,45 @@ if('serviceWorker' in navigator) {
     console.log(error);
   }
 }
+
+
+$("#menu").click(function(){
+  $(".menuderoulant").fadeIn("slow");
+  $(".fadeout").fadeIn("slow");
+});
+
+$(".fadeout").click(function(){
+  $(".menuderoulant").fadeOut("slow");
+  $(".fadeout").fadeOut("slow");
+
+});
+
+
+var map = L.map('mapid').setView([48.8577,2.3515], 13);
+map.locate({setView: true, maxZoom: 16});
+
+
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([48.8577,2.3515]).addTo(map)
+    .bindPopup('Paris')
+    .openPopup();
+
+    function onLocationFound(e) {
+        var radius = e.accuracy / 2;
+
+        L.marker(e.latlng).addTo(map)
+            .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+        L.circle(e.latlng, radius).addTo(map);
+    }
+
+    map.on('locationfound', onLocationFound);
+
+    function onLocationError(e) {
+    alert(e.message);
+}
+
+map.on('locationerror', onLocationError);
